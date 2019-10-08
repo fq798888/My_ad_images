f37026f6db52da8504b6a52837c19bc0b1ce9033
fq798888 <56250476+fq798888@users.noreply.github.com>
2019-10-08 17:39:04 +0800

    Add files via upload

---
+++ images/gg.js
@@ -0,0 +1,11 @@
+document.writeln("");
+document.writeln("<a href=\'https://www.5488s.com:8082/32\' target=\'_blank\'>");
+document.writeln("<img width=\'100%\'  style=\'border: 0px currentColor; border-image: none;\' src=\'https://www.543205.com/tu/120.gif\'  border=\'0\'></a><br>");
+document.writeln("<a target=\'_blank\' href=\'https://www.78913h.com/?f=798888\'>");
+document.writeln("<img src=\'https://cbu01.alicdn.com/img/ibank/2019/940/617/10809716049_303742291.jpg\' border=\'0\' width=\'100%\' style=\'height:90px!important;\' ></a><br>");
+document.writeln("<a target=\'_blank\' href=\'https://www.4975y.com/?f=bj789\'>");
+document.writeln("<img src=\'https://www.kj993.com/img/hf_bj.gif\' border=\'0\'  width=\'100%\' style=\'height:90px!important;\' ></a><br>");
+document.writeln("<a target=\'_blank\' href=\'https://www.h7249.com/?f=798888\'>");
+document.writeln("<img  src=\'https://cbu01.alicdn.com/img/ibank/2019/094/492/10783294490_303742291.jpg\' width=\'100%\' style=\'height:90px!important;\' border=\'0\' style=\'border: 0px;\'></a><br>");
+document.writeln("<a target=\'_blank\' href=\'https://www.hlw9966.com/?f=798888com\'>");
+document.writeln("<img  src=\'https://cbu01.alicdn.com/img/ibank/2019/454/792/10783297454_303742291.jpg\' width=\'100%\' style=\'height:90px!important;\' border=\'0\' style=\'border: 0px;\'></a>");
